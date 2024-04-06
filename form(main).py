c37e29dbd8821ab7c525ab901e2bc9471411d93c
Juhi Gupta <juhi.gupta@avantika.edu.in>
2024-04-06 10:28:22 +0000

    updated form skeleton with error checking

---
+++ form(main).py
@@ -0,0 +1,48 @@
+print("Feedback Form")
+#
+first_name=input("first name: ")
+if(first_name.isdigit()):
+    print("invalid input")
+    first_name=input("first name: ")
+#
+last_name=input("last name: ")
+if(last_name.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+email=input("email:")
+#
+contact=input("contact number:")
+#
+enroll_id=input("enrollment no:")
+#
+school==input("school:")
+if(school.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+programme=input("programme:")
+if(programme.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+discipline=input("discipline:")
+if(discipline.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+course=input("course:")
+if(course.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+course_code=input("course code")
+#
+faculty=input("faculty name:")
+if(faculty.isdigit()):
+    print("invalid input")
+    last_name=input("last name: ")
+#
+sem=input("semester:")
+
+#add feedback questions with input statement
